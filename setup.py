161ac9b354963e414ea7d93a14ea452cc2f767fc
Lars Yencken <lars@yencken.org>
2012-09-07 19:51:05 +1000

    Increment version for development.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.core import setup
 
 setup(
     name='anytop',
-    version='0.2.0',
+    version='0.2.1',
     description='Streaming frequency distribution viewer.',
     long_description=open('README.rst').read(),
     author='Lars Yencken',
